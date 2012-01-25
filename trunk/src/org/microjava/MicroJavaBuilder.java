73bf361776ea80b669ab2963f4aafd1ff7543845
djvukovic <djvukovic@abdb8f0b-8282-964f-a7e8-30747613414c>
2012-01-25 00:05:23 +0000

    Inicijalni commit

---
+++ trunk/src/org/microjava/MicroJavaBuilder.java
@@ -0,0 +1,14 @@
+package org.microjava;
+
+public class MicroJavaBuilder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 3;
+		System.out.println("microJava" + a);
+
+	}
+
+}
