04ae41ade3f9d4a4599419a9372d7e9f49e8471f
djvukovic <djvukovic@abdb8f0b-8282-964f-a7e8-30747613414c>
2012-01-29 02:43:39 +0000

    JFlex and CUP compiling. JFlex OK CUP probably

--- src/org/microjava/MicroJavaBuilder.java
+++ src/org/microjava/MicroJavaBuilder.java
@@ -1,14 +1,51 @@
 package org.microjava;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java_cup.runtime.Symbol;
+
+import org.microjava.syntax.*;
+
 public class MicroJavaBuilder {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int a = 3;
-		System.out.println("microJava" + a);
-
+		
+		FileReader inputFile = null;
+		try {
+			inputFile = new FileReader(new File("jflex_cup/program.mj"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Lexer l = new Lexer(inputFile);
+//		try {
+//			Symbol s = l.next_token();
+//			System.out.print(s.sym + " ");
+//			while(s.sym != sym.EOF)
+//			{
+//				s = l.next_token();
+//				System.out.print(s.sym + " ");
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		parser p = new parser(l);
+		try {
+			Symbol s = p.parse();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
